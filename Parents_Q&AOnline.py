224d242194ac60baf1cd0c1dbfc3c488da1c9831
Ksewovo001 <adelphesew4@gmail.com>
2025-04-19 06:45:03 -0500

    Update Parents_Q&AOnline.py

--- Parents_Q&AOnline.py
+++ Parents_Q&AOnline.py
@@ -41,17 +41,19 @@ st.markdown("""
     <p>Helping parents and families find answers, faster.</p>
 """, unsafe_allow_html=True)
 
-cols = st.columns([1, 0.2, 1]) 
+cols = st.columns([0.5, 1, 0.5, 1])
+
 try:
-    with cols[0]:
+    with cols[1]:
         image1 = Image.open("Dr_Birdiclopedia2.png")
         st.image(image1, width=200)
-    with cols[2]:
+    with cols[3]:
         image2 = Image.open("Dr_Birdiclopedia1.png")
         st.image(image2, width=200)
 except Exception as e:
     st.error("One or more images could not be loaded.")
     st.exception(e)
+
 
     
 
